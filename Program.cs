24461a43bdefc74407aab0ba2381e4624b8e8e91
botea <botea@IULIA>
2022-11-30 13:30:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public static class Globals
+{
+	internal static void Main()
+	{
+		Console.Write("Rezolvare ecuatie gradul I");
+		Console.Write("\n");
+		float a;
+		float b;
+		float x;
+		Console.Write("a = ");
+		a = Convert.ToInt32(Console.ReadLine());
+		Console.Write("b = ");
+		b = Convert.ToInt32(Console.ReadLine());
+		if (a != 0)
+		{
+			x = -b / a;
+			Console.Write("x = ");
+			Console.Write(x);
+		}
+		else
+		{
+			if (b == 0F)
+			{
+				Console.Write("infinitate de solutii");
+			}
+			else
+			{
+				Console.Write("nu are solutie");
+			}
+		}
+	}
+}
